978b126c33c7414577cf42045ce966cea1b3c222
Bogdan Khamelyuk <bogdankhamelyuk@gmail.com>
2024-02-23 20:43:35 +0100

    tried to solve android developer_error error when signin

--- firebase.config.js
+++ firebase.config.js
@@ -3,22 +3,17 @@ import { getDatabase } from "firebase/database";
 
 /* In Firebase, getAuth is a function provided by the Firebase Auth SDK that allows you to get an instance of the Auth service
 This instance enables you to perform various authentication-related operations in your application */
-import {
-  getAuth,
-  initializeAuth,
-  getReactNativePersistence,
-} from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMLFHisExbNUV2puLxpdCTDBCHhNgqLHE",
+  apiKey: "AIzaSyCloPMxVdM5sd_xyLVSPjFfZ5xv0752j4k",
   authDomain: "my-learning-app-409018.firebaseapp.com",
+  databaseURL: "https://my-learning-app-409018-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "my-learning-app-409018",
   storageBucket: "my-learning-app-409018.appspot.com",
   messagingSenderId: "861961987029",
   appId: "1:861961987029:web:1bc1c666dd60924663135c",
-  databaseURL:
-    "https://my-learning-app-409018-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
 // Initialize Firebase
